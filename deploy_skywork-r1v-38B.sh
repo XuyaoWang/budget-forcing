069fb99b1fd315e14ba2155bc38df923ca01312d
bearsensei <clark.zhang2008@gmail.com>
2025-04-19 21:39:35 +0800

    local path

--- deploy_skywork-r1v-38B.sh
+++ deploy_skywork-r1v-38B.sh
@@ -12,7 +12,7 @@
 
 
 
-python -m vllm.entrypoints.openai.api_server --model Skywork/Skywork-R1V-38B \
+python -m vllm.entrypoints.openai.api_server --model /ceph/home/yaodong01/s1-m/Models/Skywork-R1V-38B/ \
                                                     --port 8000 \
                                                     --tensor-parallel-size 4 \
                                                     --gpu-memory-utilization 0.8 \
