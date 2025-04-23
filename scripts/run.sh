ea7655a0742850ec8c93f3e821be426710fb144a
Xuyao Wang <71309362+XuyaoWang@users.noreply.github.com>
2025-04-23 23:04:04 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export VLLM_USE_V1=0
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+# export CUDA_VISIBLE_DEVICES=4,5,6,7
 # Set the project root directory
 PROJECT_ROOT="."
 
@@ -9,9 +9,11 @@ MODEL_PATH=Qwen/Qwen2.5-VL-3B-Instruct
 MODEL_NAME=qwen-vl-3b
 PORT=8010
 HOST=localhost
+PP_SIZE=1
 TP_SIZE=4
-GPU_UTILIZATION=0.6
+GPU_UTILIZATION=0.95
 LIMIT_MM_PER_PROMPT="image=10,video=10"
+MAX_SEQ_LEN=32768
 CHAT_TEMPLATE=./scripts/qwen2_5_vl_test-scaling.jinja
 ENABLE_PREFIX_CACHING=true
 DTYPE=bfloat16
@@ -44,9 +46,11 @@ python main.py \
   --model-name ${MODEL_NAME} \
   --port ${PORT} \
   --host ${HOST} \
+  --pipeline-parallel-size ${PP_SIZE} \
   --tensor-parallel-size ${TP_SIZE} \
   --gpu-memory-utilization ${GPU_UTILIZATION} \
   --limit-mm-per-prompt "${LIMIT_MM_PER_PROMPT}" \
+  --max-seq-len ${MAX_SEQ_LEN} \
   --dtype ${DTYPE} \
   --benchmark ${BENCHMARK} \
   --data-path ${DATA_PATH} \
