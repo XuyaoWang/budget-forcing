265169321bb78f4d8d6587691c7b931ef4853a87
W3en2g <pc.wen@outlook.com>
2025-04-29 13:41:01 +0800

    less cpu try

--- derun_skywork-r1v-38Bv2.sh
+++ derun_skywork-r1v-38Bv2.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=IAI_SLURM_HGX                # 分区
 #SBATCH --nodes=1                      # 节点数
 #SBATCH --gres=gpu:8                   # 每个节点使用的 GPU 数量
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=50
 #SBATCH --ntasks-per-node=2
 #SBATCH --mem=500GB
 #SBATCH --qos=12gpu-hgx 
